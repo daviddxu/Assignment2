071b3f13d236919e248adc53d208ef154c095cdb
davepierre23 <32973814+davepierre23@users.noreply.github.com>
2017-10-23 18:34:04 -0400

    Update word_server.js

--- word_server.js
+++ word_server.js
@@ -3,6 +3,8 @@
 //import fs  to read and writeTOFile
 var fs = require('fs');
 
+
+
 var http = require('http'); //need to http
 var url = require('url');  //to parse url strings
 
@@ -88,147 +90,7 @@ http.createServer(function (request,response){
 		   var dataObj = JSON.parse(receivedData);
            console.log('received data object: ', dataObj);
            console.log('type: ', typeof dataObj);
-		   //Here we can decide how to process the data object and what
-		   //object to send back to client.
-		   //FOR NOW EITHER JUST PASS BACK AN OBJECT
-		   //WITH "text" PROPERTY
-
-		   /*
-			if return object is empty, return "NOT FOUND" to client
-		   */
-
-			if(dataObj.text == "Sister Golden Hair"){
-				dataObj.title = "Sister Golden Hair";
-			}
-			if(dataObj.text == "Brown Eyed Girl"){
-				dataObj.title = "Brown Eyed Girl";
-			}
-			if(dataObj.text == "Peaceful Easy Feeling"){
-				dataObj.title = "Peaceful Easy Feeling";
-			}
-
-			if(dataObj.title == "Sister Golden Hair"){
-
-				if(dataObj.type == "update"){
- 					//(1) update sisterGoldenHair array (2) "Sister Golden Hair.txt";
-					array = dataObj.wordArray;
-					for(let i = 0; i < array; i++){
-						console.log(array.word, array.x, array.y);
-					}
-					sisterGoldenHair = [];
-					for(let i = 0; i < array.length; i++){
-						sisterGoldenHair[i] = array[i];
-					}
-
-					//file write
-
-				var a_str = "";
-
-				for(let i = 0; i < sisterGoldenHair.length; i++){
-					a_str+=sisterGoldenHair[i];
-				}
-
-				}
-
-				var returnObj = {};
-
-				returnObj.wordArray = sisterGoldenHair;
-
-        fs.writeFile('songs/Sister Golden Hair.txt', dataObj.stringText, function(err) {
-          if(err) throw err;
-
-        });
-
-
-
-
-
-
-				response.writeHead(200, {'Content-Type': MIME_TYPES["text"]});  //does not work with application/json MIME
-					response.end(JSON.stringify(returnObj)); //send just the JSON object
-
-
-
-				}
-
-	 	if(dataObj.title == "Brown Eyed Girl"){
-				if(dataObj.type == "update"){
- 					//(1) update brownEyedGirl array (2) "Brown Eyed Girl.txt";
-					array = dataObj.wordArray;
-					for(let i = 0; i < array; i++){
-						console.log(array.word, array.x, array.y);
-					}
-					brownEyedGirl = [];
-					for(let i = 0; i < array.length; i++){
-						brownEyedGirl[i] = array[i];
-					}
-
-				var a_str = "";
-
-				for(let i = 0; i < brownEyedGirl.length; i++){
-					a_str+=brownEyedGirl[i];
-				}
-
-				}
-
-				var returnObj = {};
-
-						returnObj.wordArray = brownEyedGirl;
-            fs.writeFile('songs/Brown Eyed Girl.txt', dataObj.stringText, function(err) {
-              if(err) throw err;
-
-            });
-
-						response.writeHead(200, {'Content-Type': MIME_TYPES["text"]});  //does not work with application/json MIME
-						response.end(JSON.stringify(returnObj)); //send just the JSON object
-
-
-			}
-
-	 	if(dataObj.title == "Peaceful Easy Feeling"){
-
-		if(dataObj.type == "update"){
- 					//(1) update peacefulEasyFeeling array (2) "Peaceful Easy Feeling.txt";
-					array = dataObj.wordArray;
-					for(let i = 0; i < array; i++){
-						console.log(array.word, array.x, array.y);
-					}
-					peacefulEasyFeeling = [];
-					for(let i = 0; i < array.length; i++){
-						peacefulEasyFeeling[i] = array[i];
-					}
-
-				var a_str = "";
-
-				for(let i = 0; i < peacefulEasyFeeling.length; i++){
-					a_str+=peacefulEasyFeeling[i];
-				}
-				}
-
-				var returnObj = {};
-
-				returnObj.wordArray = peacefulEasyFeeling;
-				response.writeHead(200, {'Content-Type': MIME_TYPES["text"]});  //does not work with application/json MIME
-				response.end(JSON.stringify(returnObj)); //send just the JSON object
-
-
-			}
-		   //TO DO: return the words array that the client requested
-		   //if it exists
-
-		   else{console.log("USER REQUEST: " + dataObj.text );
-		   var returnObj = {};
-		   returnObj.title = dataObj.title;
-		   returnObj.text = 'NOT FOUND: ' + dataObj.text;
-		   }
-
-       //file write to
-
-
-       fs.writeFile('songs/Peaceful Easy Feeling.txt', dataObj.stringText, function(err) {
-         if(err) throw err;
-
-       });
+		  
 		   //object to return to client
           response.writeHead(200, {'Content-Type': MIME_TYPES["text"]});  //does not work with application/json MIME
            response.end(JSON.stringify(returnObj)); //send just the JSON object
@@ -238,7 +100,7 @@ http.createServer(function (request,response){
      if(request.method == "GET"){
 	 //handle GET requests as static file requests
 	 var filePath = ROOT_DIR + urlObj.pathname;
-	 if(urlObj.pathname === '/') filePath = ROOT_DIR + '/assign1.html';
+	 if(urlObj.pathname === '/') filePath = ROOT_DIR + '/assignment2.html';
 
      fs.readFile(filePath, function(err,data){
        if(err){
